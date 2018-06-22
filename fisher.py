0d29a2a605f26d89353ebc4768329085e5321b00
aclstack <aclstack@gmail.com>
2018-06-22 11:30:46 +0800

    增加简单响应header方法

--- fisher.py
+++ fisher.py
@@ -8,14 +8,22 @@ app.config.from_object('config')
 @app.route('/')
 def index():
     headers = {
-        'content-type': 'application/json'
-       # 'location': 'https://www.baidu.com'  #跳转
+        'content-type': 'application/json'   # 返回类型指定为json
+        # 'location': 'https://www.baidu.com' #跳转
     }
 
-    response = make_response('<html></html>')
+    response = make_response('<html></html>',301)
     response.headers = headers
     return response
-    # return "Hello World"
+
+
+@app.route('/easy')
+def index():
+    headers = {
+        'content-type': 'application/json'   # 指定客户端解析类型
+    }
+
+    return 'easy response', 200, headers
 
 
 app.run(debug=app.config["DEBUG"])
