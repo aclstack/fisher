4cdac9f55fa535460e9c98faec37210c8e4a8e7d
aclstack <aclstack@gmail.com>
2018-06-30 08:53:25 +0800

    增加表单验证wtform功能

--- app/web/book.py
+++ app/web/book.py
@@ -2,17 +2,23 @@
 from flask import jsonify, request
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
+from app.forms.book import SearchForm
 from . import web
 
 
 @web.route('/book/search/')
 def search():
-    q = request.args['q']
-    page = request.args['page']
 
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
+    # 验证层，对参数进行校验
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q)
+        return jsonify(result)  # flask自带的返回json
     else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)  # flask自带的返回json
+        return jsonify({'msg': '参数校验失败'})
