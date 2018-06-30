6416e6e5707ab1bc6cfe9817f9f661be6fd164b2
aclstack <aclstack@gmail.com>
2018-06-30 08:23:46 +0800

    获取request参数

--- app/web/book.py
+++ app/web/book.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import jsonify
+from flask import jsonify, request
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 from . import web
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search/')
+def search():
+    q = request.args['q']
+    page = request.args['page']
+
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = YuShuBook.search_by_isbn(q)
